c82d435190b766538f6dd96a603ed22b00c7ebb8
ken8 <ksamemail@gmail.com>
2025-01-29 11:19:59 -0800

    Initial commit from your app

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { SplineSceneBasic } from '../components/ui/code.demo.tsx'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <SplineSceneBasic />
+  </React.StrictMode>,
+)
